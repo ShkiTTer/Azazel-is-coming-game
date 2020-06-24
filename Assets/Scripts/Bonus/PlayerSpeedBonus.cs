b7641fa7fb028ef7ff0bef25aea33be422489a07
ShkiTTer <shkipper65@gmail.com>
2020-06-24 17:45:55 +0700

    Add player speed and firing rate bonuses

---
+++ Assets/Scripts/Bonus/PlayerSpeedBonus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Bonus
+{
+    public class PlayerSpeedBonus: Bonus<float>
+    {
+        public override float BonusValue { get; }
+        public override BonusType Type { get; } = BonusType.PlayerSpeed;
+    }
+}
