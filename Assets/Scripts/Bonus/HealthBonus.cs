c2a71221a5ceb022d44e335e79a7c252e06010d8
ShkiTTer <shkipper65@gmail.com>
2020-06-26 21:13:40 +0700

    Add bonus prefabs

--- Assets/Scripts/Bonus/HealthBonus.cs
+++ Assets/Scripts/Bonus/HealthBonus.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts.Bonus
 {
     public class HealthBonus: Bonus<Int32>
     {
-        public override int BonusValue { get; } = 1;
+        [SerializeField]
+        private int _bonusValue = 1;
+        public override int BonusValue => _bonusValue;
         public override BonusType Type { get; } = BonusType.Health;
     }
 }
--- Assets/Scripts/Bonus/PlayerSpeedBonus.cs
+++ Assets/Scripts/Bonus/PlayerSpeedBonus.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts.Bonus
 {
     public class PlayerSpeedBonus: Bonus<float>
     {
-        public override float BonusValue { get; }
+        [SerializeField]
+        private float _bonusValue;
+
+        public override float BonusValue => _bonusValue;
         public override BonusType Type { get; } = BonusType.PlayerSpeed;
     }
 }
